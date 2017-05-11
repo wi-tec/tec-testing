4c4df90952119e8046568a702adb7ec4777e98de
Marvin J. Cortés <marvin.cortes@hotmail.com>
2017-05-11 08:36:00 -0600

    Rama de Marvin, archivos editados

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,44 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_BackstopJS_Homepage_0_l-sidebar-second_0_phone.png",
+        "test": "../bitmaps_test/20170510-143358/prod_test_BackstopJS_Homepage_0_l-sidebar-second_0_phone.png",
+        "selector": ".l-sidebar-second",
+        "fileName": "prod_test_BackstopJS_Homepage_0_l-sidebar-second_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {},
+        "error": {}
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_BackstopJS_Homepage_0_l-sidebar-second_1_tablet_v.png",
+        "test": "../bitmaps_test/20170510-143358/prod_test_BackstopJS_Homepage_0_l-sidebar-second_1_tablet_v.png",
+        "selector": ".l-sidebar-second",
+        "fileName": "prod_test_BackstopJS_Homepage_0_l-sidebar-second_1_tablet_v.png",
+        "label": "BackstopJS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {},
+        "error": {}
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_BackstopJS_Homepage_0_l-sidebar-second_2_tablet_h.png",
+        "test": "../bitmaps_test/20170510-143358/prod_test_BackstopJS_Homepage_0_l-sidebar-second_2_tablet_h.png",
+        "selector": ".l-sidebar-second",
+        "fileName": "prod_test_BackstopJS_Homepage_0_l-sidebar-second_2_tablet_h.png",
+        "label": "BackstopJS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {},
+        "error": {}
+      },
+      "status": "fail"
+    }
+  ]
+});
---
+++ tests/behat/features/sugerencias.feature
@@ -0,0 +1,10 @@
+Feature: Suggestions box.
+  In order provide feedback
+  As a user
+  I need to able to submit the suggestions form.
+
+  Scenario: submit suggestions form.
+    Given I am an anonymous user
+    When I am at "formularios/buzon-sugerencias"
+    And I fill in
+
