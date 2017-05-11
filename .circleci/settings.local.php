40137c5c2f540677f7f2dbe42e7f9709f5f1e805
Roy Cordero <roy.cordero1209@gmail.com>
2017-05-11 10:31:38 -0600

    Add change to circle config

--- .circleci/settings.local.php
+++ .circleci/settings.local.php
@@ -7,32 +7,5 @@ $databases['default']['default'] = array(
   'username' => 'root',
   'password' => 'drupal',
   'driver' => 'mysql',
-  'host'   => 'localhost',
+  'host'   => '127.0.0.1',
 );
-
-// Variable overrides
-//----------------------
-
-// Anonymous caching
-// $conf['cache'] = 0;
-
-// Block caching
-// $conf['block_cache'] = 0;
-
-// Cache lifetime
-// $conf['cache_lifetime'] = 0;
-
-// Expiration of cached pages
-// $conf['page_cache_maximum_age'] = 0;
-
-// Aggregation/Compression of CSS files
-// $conf['preprocess_css'] = 0;
-
-// Aggregation/Compression of JavaScript files
-// $conf['preprocess_js'] = 0;
-
-// Page compression
-// $conf['page_compression'] = 0;
-
-// Theme debugging
-// $conf['theme_debug'] = 1;
