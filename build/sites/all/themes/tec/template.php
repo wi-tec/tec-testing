65bc087cdc9cb479f3ffbfcf745ee62be5bfc207
Marvin J. Cortés <marvin.cortes@hotmail.com>
2018-03-22 15:11:58 -0600

    Agrega pantheon_rsa key

--- build/sites/all/themes/tec/template.php
+++ build/sites/all/themes/tec/template.php
@@ -7,6 +7,7 @@
  */
 function tec_css_alter(&$css) {
   $exclude = array(
+  
   // Remove Drupal Core CSS
     'modules/aggregator/aggregator.css' => FALSE,
     //'modules/block/block.css' => FALSE,
